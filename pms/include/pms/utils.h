8aefa1b9af3b44860a3c8f0f3434f6c73b9e40a6
neverorfrog <97flavio.maiorana@gmail.com>
2025-05-25 20:18:41 +0200

    added clean format

--- pms/include/pms/utils.h
+++ pms/include/pms/utils.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #include <Eigen/Dense>
+#include "Eigen/Core"
 
 namespace pms {
 inline Eigen::IOFormat PythonFmt(Eigen::StreamPrecision, Eigen::DontAlignCols, ", ", "; ", "", "", "[", "]");
+inline Eigen::IOFormat CleanFmt(4, 0, " ", "\n", "", "");
 }
